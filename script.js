168dd84cedabdf352a8b9f4c3a345c251a42c009
eskainah <eskainah@gmail.com>
2024-01-26 12:26:41 +0200

    update computer choice function

--- script.js
+++ script.js
@@ -2,13 +2,18 @@
 function getComputerChoice(){
     const randomNum = Math.floor(Math.random() * 3);
     let computerChoice;
-    if (randomNum === 1){
+    if (randomNum === 0){
         computerChoice = "Rock";
+        console.log(randomNum);
     }
-    else if (randomNum === 2){
+    else if (randomNum === 1){
         computerChoice = "Paper";
+        console.log(randomNum);
     }
     else {
         computerChoice = "Scissor"
+        console.log(randomNum);
     }
+    return computerChoice
 }
+console.log(getComputerChoice())
