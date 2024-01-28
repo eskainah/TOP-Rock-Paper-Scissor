4fcbdd4d86be612f8f5656ee3c082c26ba24a826
eskainah <eskainah@gmail.com>
2024-01-28 03:19:10 +0200

    organize code

--- script.js
+++ script.js
@@ -1,33 +1,27 @@
+let opt = ['rock', 'paper', 'scissor']; // an array of options
+let result = ['Player Win', 'Player Lost', 'Tie']; // an array of result per round
+let playerSelection, msg;
+
 /**
  * A function that randomly return computer choice 
  * it assign the value to a value in the array choice
 */
-let opt = ['rock', 'paper', 'scissor']
-let playerScore;
-let computerScore;
-let result = ['Player Win', 'Player Lost', 'Tie'];
-let winner;
-let msg;
-
-
-
-
 function getComputerChoice(){
    
     let computerChoice;
     const randomNum = Math.floor(Math.random()*opt.length);
-    console.log("the random num is ")
+    
     if (randomNum === 0){
         computerChoice = opt[0];
-        alert(computerChoice);
+        console.log("the random num is ", computerChoice )
     }
     else if (randomNum=== 1){
         computerChoice = opt[1];
-        console.log(computerChoice);
+        console.log("the random num is ", computerChoice )
     }
     else{
         computerChoice = opt[2];
-        console.log(computerChoice);
+        console.log("The random num is ", computerChoice);
     }
     return computerChoice; 
 }
@@ -38,83 +32,108 @@ function getComputerChoice(){
  * return a string that declare the winner
  * it compares the string input by user length
  */
-let playerSelection;
- function playRound(playerSelection, computerSelection){
-    playerSelection = "rock";
-   
-    playerSelection = playerSelection.toLowerCase()
 
-    if(playerSelection === computerSelection){ //works
+ function playRound(playerSelection, computerSelection){
+
+    if(playerSelection === computerSelection){
         msg = result[2];
     } 
     
-    else if(playerSelection == "scissor" && computerSelection == "rock"){ //works
+    else if(playerSelection == opt[2] && computerSelection == opt[0]){
         msg = result[1]; 
     } 
-    else if(playerSelection == "rock" && computerSelection == "scissor"){ //works
+    else if(playerSelection == opt[0] && computerSelection == opt[2]){
         msg = result[0];
     } 
 
-    else if(playerSelection == "rock" && computerSelection == "paper"){ //works
+    else if(playerSelection == opt[0] && computerSelection == opt[1]){
         msg = result[1];
     } 
-    else if(playerSelection == "paper" && computerSelection == "rock"){ //works
+    else if(playerSelection == opt[1] && computerSelection == opt[0]){
         msg = result[0];
     } 
 
-    else if(playerSelection == "paper" && computerSelection == "scissor"){ //works
+    else if(playerSelection == opt[1] && computerSelection == opt[2]){
         msg = result[1];
     } 
-    else if(playerSelection == "scissor" && computerSelection == "paper"){ //works
+    else if(playerSelection == opt[2] && computerSelection == opt[1]){ 
         msg = result[0];
     } 
-    
     return msg;
 }
-document.write("<br/>",playRound(playerSelection, getComputerChoice()));
-
 
 function scores(){
-    playerScore = 0;
-    computerScore = 0;
-   
+    
     if (msg === result[0]){
-        playerScore ++;
-        
+        playerScore ++; 
     }
     else if (msg === result[1]){
         computerScore ++;
-        
     }
     else{
         //alert(result[2]);
     }
     return playerScore, computerScore;
 }
-scores();
-console.log(playerScore)
-console.log(computerScore)
+
 
 function getWinner(){
+    let winner;
+
     if(playerScore > computerScore){
-        winner = "Player";
+        winner = "Bravo player you won";
     }
     else if (computerScore> playerScore){
-        winner = "Computer"
+        winner = "Oops! Computer wins"
     }
     else{
-        winner = "tie"
+        winner = "Execellence Game, we have a tie"
+    }
+    return winner;
+}
+
+/** 
+ * 
+ * */ 
+function gameProgress(){
+    const computerSelection = getComputerChoice();
+    roundResult = playRound(playerSelection, computerSelection);
+    console.log(roundResult)
+   
+    scores();
+}
+
+function game(){
+    let roundResult;
+
+    playerScore = 0;
+    computerScore = 0;
+
+    for(let i = 0; i < 5; i++){
+        playerSelection = prompt("Rock, Paper, Scissor");
+        playerSelection = playerSelection.toLowerCase()
+
+        if (playerSelection != opt[0]){
+            if (playerSelection != opt[1]){
+                if (playerSelection != opt[2]){
+                    break;
+                }
+                else{
+                    gameProgress();
+                }
+            } 
+            else{
+                gameProgress();
+            }
+        }
+        else{
+            gameProgress();  
+        }
     }
 }
-getWinner();
-console.log(winner)
-
-/* function game(){
-    let playerSelection
-
-    for(let i =0; i<5; i++){
-        playerSelection = prompt("Rock, Paper. Scissor ");
-    }
-    return playerSelection;
-}
-game(); */
+game(); 
+console.log(playerScore)
+/* document.write("<br/>", );
+document.write("<br/>", );
+*/
+document.write("The Computer Score is: ", computerScore, "<br/>", "The Player Score is: ", playerScore, "<br/>", getWinner());
